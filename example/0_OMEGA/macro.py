7c4a7f882e88b4bb5422a66aec47f1e30cb30189
Lennard94 <34376186+Lennard94@users.noreply.github.com>
2021-05-10 14:21:46 +0200

    Add files via upload

---
+++ example/0_OMEGA/macro.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# (C) 2017 OpenEye Scientific Software Inc. All rights reserved.
+#
+# TERMS FOR USE OF SAMPLE CODE The software below ("Sample Code") is
+# provided to current licensees or subscribers of OpenEye products or
+# SaaS offerings (each a "Customer").
+# Customer is hereby permitted to use, copy, and modify the Sample Code,
+# subject to these terms. OpenEye claims no rights to Customer's
+# modifications. Modification of Sample Code is at Customer's sole and
+# exclusive risk. Sample Code may require Customer to have a then
+# current license or subscription to the applicable OpenEye offering.
+# THE SAMPLE CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED.  OPENEYE DISCLAIMS ALL WARRANTIES, INCLUDING, BUT
+# NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. In no event shall OpenEye be
+# liable for any damages or liability in connection with the Sample Code
+# or its use.
+
+import sys
+from openeye import oechem
+from openeye import oeomega
+
+
+def main(argv=[__name__]):
+    if len(argv) != 3:
+        oechem.OEThrow.Usage("%s <infile> <outfile>" % argv[0])
+
+    ifs = oechem.oemolistream()
+    if not ifs.open(argv[1]):
+        oechem.OEThrow.Fatal("Unable to open %s for reading" % argv[1])
+
+    ofs = oechem.oemolostream()
+    if not ofs.open(argv[2]):
+        oechem.OEThrow.Fatal("Unable to open %s for writing" % argv[2])
+
+    if not oechem.OEIs3DFormat(ofs.GetFormat()):
+        oechem.OEThrow.Fatal("Invalid output file format for 3D coordinates!")
+
+    omegaOpts = oeomega.OEMacrocycleOmegaOptions()
+    mcomega = oeomega.OEMacrocycleOmega(omegaOpts)
+    for mol in ifs.GetOEMols():
+        oechem.OEThrow.Info("Title: %s" % mol.GetTitle())
+        ret_code = mcomega.Build(mol)
+        if ret_code == oeomega.OEOmegaReturnCode_Success:
+            oechem.OEWriteMolecule(ofs, mol)
+        else:
+            oechem.OEThrow.Warning("%s: %s" % (mol.GetTitle(), oeomega.OEGetOmegaError(ret_code)))
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
