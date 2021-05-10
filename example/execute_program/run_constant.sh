6135afcf6b4e6142f1b63bca1aaf35e57442045a
Lennard94 <34376186+Lennard94@users.noreply.github.com>
2021-05-10 18:42:40 +0200

    Update run_constant.sh

--- example/execute_program/run_constant.sh
+++ example/execute_program/run_constant.sh
@@ -2,7 +2,7 @@ lb=500
 hb=1000
 c=20 ## change to different value if desired (cutoff in scoring function)
 s0=0.1
-file=IR_constant_baseline_500_1000
+file=IR_constant_baseline_500_1800
 for w in 12; do #change if desired
     for mu in 1.000; do # change if desired
     python Main.py -lb ${lb} -hb ${hb} -e I_long_correct/gibbs.p --freq I_long_correct/freq.p -exp ${file} -o I_${w}_${lb}_${hb}_${c}_${mu}_constant_baseline -w1 $w -w2 $w -s0 $s0 -s1 0.0192 -c ${c} -m ${mu} -d 1 --peaks peaks_500_1000
