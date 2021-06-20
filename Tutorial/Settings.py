d10e44111037f79b0bd922211f67967454e28563
Lennard94 <34376186+Lennard94@users.noreply.github.com>
2021-06-20 16:33:54 +0200

    Update Settings.py

--- Tutorial/Settings.py
+++ Tutorial/Settings.py
@@ -10,9 +10,12 @@ class Settings:
         self.sigma_2=0.1
         self.x_min=1000
         self.x_max=1500
+        self.w=12
     def get(self):
         return self.shift_factor, self.cutoff_absolute, self.cutoff, self.sigma_1, self.sigma_2, self.use_vcd, self.x_min, self.x_max
     def get_directory(self):
         return self.directory
+    def get_w(self):
+        return self.w
 
 
